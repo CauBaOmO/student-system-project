078e9f476294712378d540a2a93ccab7d0f37d58
ADLO2 <nmthuan21+github@clc.fitus.edu.vn>
2022-03-18 21:10:22 +0700

    List out school years available

--- functions.h
+++ functions.h
@@ -1,7 +1,10 @@
 #pragma once
-
+#include<dirent.h>
+#include<sys\stat.h>
+#include<conio.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 using namespace std;
 
+void listOFYear(string initStrPath);
